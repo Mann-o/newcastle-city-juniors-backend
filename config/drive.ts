9840ad283fcd3d0126b33a63498a11f1797cf6bd
Liam Potter <liam-p@fostr.io>
2022-07-02 01:32:29 +0100

    restore folder?

--- config/drive.ts
+++ config/drive.ts
@@ -11,7 +11,7 @@ export default driveConfig({
       visibility: 'public',
       root: Application.tmpPath('uploads'),
       serveFiles: true,
-      basePath: '/',
+      basePath: '/uploads',
     },
   },
 })

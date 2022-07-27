d930e2bcc769186057f4da2d7885f66de8fd3d93
Liam Potter <liam-p@fostr.io>
2022-07-27 08:56:08 +0100

    wip

--- app/Controllers/Http/Admin/PlayerController.ts
+++ app/Controllers/Http/Admin/PlayerController.ts
@@ -1,7 +1,11 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+// import Env from '@ioc:Adonis/Core/Env'
+
+// import Stripe from 'stripe'
 
 import Player from 'App/Models/Player'
 import Parent from 'App/Models/Parent'
+// import User from 'App/Models/User'
 
 export default class PlayerController {
   public async getAllPlayers({ auth, response }: HttpContextContract) {
@@ -25,24 +29,35 @@ export default class PlayerController {
   }
 
   public async getParentForPlayer({ auth, response, params }: HttpContextContract) {
-    const user = auth.use('api').user!
+    const authUser = auth.use('api').user!
 
     const requiredPermissions = ['staff', 'view-players'];
-    const userPermissions = (await user!.related('permissions').query()).map(({ name }) => name)
+    const userPermissions = (await authUser!.related('permissions').query()).map(({ name }) => name)
     const hasRequiredPermissions = requiredPermissions.every(requiredPermission => userPermissions.includes(requiredPermission));
 
     if (!hasRequiredPermissions) {
       return response.unauthorized()
     }
 
-    const player = await Player.query().where({ id: params.playerId })
+    // const stripeClient = new Stripe(Env.get('STRIPE_API_SECRET', null), {
+    //   apiVersion: Env.get('STRIPE_API_VERSION'),
+    // })
 
-    const parent = await Parent.query().where({ id: player[0].parentId })
+    const player = await Player.query().where({ id: params.playerId }).firstOrFail()
+
+    const parent = await Parent.query().where({ id: player.parentId }).firstOrFail()
+
+    // const user = await User.query().where({ id: player.userId }).firstOrFail()
+
+    // const { data: charges } = await stripeClient.charges.list({ customer: user.stripeCustomerId })
 
     return response.ok({
       status: 'OK',
       code: 200,
-      data: parent[0],
+      data: {
+        parent,
+        // charges,
+      },
     })
   }
 

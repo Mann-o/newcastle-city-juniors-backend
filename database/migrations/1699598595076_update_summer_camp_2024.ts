3161a9b9f2e76e4a58233966f2d8c7b90436c9fc
Liam Potter <me@liam-potter.co.uk>
2024-03-24 21:17:39 +0000

    fix table name

--- database/migrations/1699598595076_update_summer_camp_2024.ts
+++ database/migrations/1699598595076_update_summer_camp_2024.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'summer_cup_2024'
+  protected tableName = 'summer_cup_2024_signups'
 
   public async up () {
     this.schema.alterTable(this.tableName, table => {

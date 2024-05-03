759e1317a8084cd5d4e8942fc00e28e0150537b7
Liam Potter <me@liam-potter.co.uk>
2024-05-03 23:12:16 +0100

    add presentation tickets counts

--- app/Controllers/Http/Admin/PlayerController.ts
+++ app/Controllers/Http/Admin/PlayerController.ts
@@ -184,81 +184,22 @@ export default class PlayerController {
   }
 
   public async getPresentationTicketsPaymentSchedule({ auth, response }: HttpContextContract) {
-
     const user = auth.use('api').user!
 
     const requiredPermissions = ['staff', 'view-payments'];
     const userPermissions = (await user!.related('permissions').query()).map(({ name }) => name)
-    const hasRequiredPermissions = requiredPermissions.every(requiredPermission => userPermissions.includes(requiredPermission));
+    const hasRequiredPermissions = requiredPermissions.every(requiredPermission => userPermissions.includes(requiredPermission)) || userPermissions.includes('sudo');
 
     if (!hasRequiredPermissions) {
       return response.unauthorized()
     }
 
-    const paymentIntents: any[] = [];
-    const orders: any[] = [];
-
-    const playerTickets = {
-      id: 'price_1N2sgiJgy48auTmo5gZVPEEb',
-      total: 0,
-    }
-
-    const visitorTickets = {
-      id: 'price_1N2sgdJgy48auTmo0aouX7Kk',
-      total: 0,
-    }
-
-    const stripeClient = new Stripe(Env.get('STRIPE_API_SECRET', null), {
-      apiVersion: Env.get('STRIPE_API_VERSION'),
-    })
-
-    for await (const paymentIntent of stripeClient.paymentIntents.search({
-      query: 'amount<=1000 AND status:"succeeded" AND created>1682895600',
-      limit: 100,
-    })) {
-      paymentIntents.push(paymentIntent.id);
-    }
-
-    for (const paymentIntent of paymentIntents) {
-      const checkout: any = await stripeClient.checkout.sessions.list({
-        payment_intent: paymentIntent,
-        expand: ['data.line_items'],
-      });
-
-      if (checkout.data[0].status === 'complete') {
-        const order = {
-          created: checkout.data[0].created,
-          customer: checkout.data[0].customer_details.email,
-          tickets: {
-            player: 0,
-            visitor: 0,
-          },
-          player: checkout.data[0].metadata.presentationTicketPlayerName,
-          team: checkout.data[0].metadata.presentationTicketTeam,
-        };
-
-        if (checkout.data[0].line_items.data.some((item: any) => item.price.id === playerTickets.id)) {
-          playerTickets.total += checkout.data[0].line_items.data.filter((item: any) => item.price.id === playerTickets.id)[0].quantity;
-          order.tickets.player = checkout.data[0].line_items.data.filter((item: any) => item.price.id === playerTickets.id)[0].quantity;
-        }
-
-        if (checkout.data[0].line_items.data.some((item: any) => item.price.id === visitorTickets.id)) {
-          visitorTickets.total += checkout.data[0].line_items.data.filter((item: any) => item.price.id === visitorTickets.id)[0].quantity;
-          order.tickets.visitor = checkout.data[0].line_items.data.filter((item: any) => item.price.id === visitorTickets.id)[0].quantity;
-        }
-
-        orders.push(order);
-      }
-    };
+    const orders = await Database.from('presentation_2023_2024').orderBy('id', 'asc')
 
     return response.ok({
       status: 'OK',
       code: 200,
       data: {
-        totals: {
-          player: playerTickets.total,
-          visitor: visitorTickets.total,
-        },
         orders,
       },
     })
